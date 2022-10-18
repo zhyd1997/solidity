9e9b3e23122f8d5d995f2c2cd3c9d54fec762c67
Yadong Zhang <zhyd007@gmail.com>
2022-10-18 09:59:14 +0800

    feat: added SetLib contract. (#24)

---
+++ contracts/SetLib.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >=0.6.0 <0.9.0;
+
+struct Data {
+  mapping (uint => bool) flags;
+}
+
+library Set {
+  function insert(Data storage self, uint value) public returns (bool) {
+    if (self.flags[value]) {
+      return false;
+    }
+    self.flags[value] = true;
+    return true;
+  }
+
+  function remove(Data storage self, uint value) public returns (bool) {
+    if (!self.flags[value]) {
+      return false;
+    }
+    self.flags[value] = false;
+    return true;
+  }
+
+  function contains(Data storage self, uint value) public view returns (bool) {
+    return self.flags[value];
+  }
+}
+
+contract SetLib {
+  Data knownValues;
+
+  function register(uint value) public {
+    require(Set.insert(knownValues, value));
+  }
+}
